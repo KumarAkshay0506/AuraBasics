3894399d4205957ab9c8ad356c94324a2f723b8f
Mitch Fish <mfishco@gmail.com>
2019-04-07 16:51:39 -0600

    After Completing Server Side Controllers

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,4 +1,23 @@
 ({
+    //Load Expenses from Salesforce
+    doInit : function(component, event, helper) {
+        //create the Action
+        var action = component.get("c.getItems");
+        //add a callback fron when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+                console.log("ReturnValue: " + response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        //Send action off to be executed
+        $A.enqueueAction(action);
+    },
+    
     clickCreateItem : function(component, event, helper) {
         var validItem = component.find('campingForm').reduce(function (validSoFar, inputCmp) {
             //displays error messages for invalid fields
@@ -9,16 +28,7 @@
             //create the camping item
             var newItem = component.get("v.newItem");
             console.log("Create Camping Item: " + JSON.stringify(newItem));  //todo remove after testing
-            var campingItems = component.get("v.items");
-            var anItem = JSON.parse(JSON.stringify(newItem));
-            campingItems.push(anItem);
-            component.set("v.items", campingItems);
-            component.set("v.newItem", {'sObjectType' : 'Camping_Item__c',
-                                        'Name' : '',
-                                        'Quantity__c' : 0,
-                                        'Price__c' : 0,
-                                        'Packed__c' : false })
-
+            helper.createItem(component, newItem);
         }
     }
 })
--- force-app/main/default/aura/campingList/campingListHelper.js
+++ force-app/main/default/aura/campingList/campingListHelper.js
@@ -1,5 +1,20 @@
 ({
-    helperMethod : function() {
-
+    createItem : function(component, item) {
+        var action = component.get("c.saveItem");
+        action.setParams({
+            "item": item
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            console.log("State: " + state);
+            if (state === "SUCCESS") {
+                console.log("Success: " + items);
+                var items = component.get("v.items");
+                items.push(response.getReturnValue());
+                component.set("v.items", items);
+                console.log("Success: " + items);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
