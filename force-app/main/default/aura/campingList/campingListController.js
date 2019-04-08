4db8a24af5fbdf1a79d0890f9853bbca733ae97b
Mitch Fish <mfishco@gmail.com>
2019-04-08 17:12:13 -0600

    Solved using vindaloo but not sure why.

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,38 +1,38 @@
 ({
-    //Load Expenses from Salesforce
-    doInit : function(component, event, helper) {
-        //create the Action
+    // Load expenses from Salesforce
+    doInit: function(component, event, helper) {
+        
+        // Create the action
         var action = component.get("c.getItems");
-        //add a callback fron when response is received
+        
+        // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
-            if(state === "SUCCESS") {
+            if (state === "SUCCESS") {
                 component.set("v.items", response.getReturnValue());
-                console.log("ReturnValue: " + response.getReturnValue());
             }
             else {
                 console.log("Failed with state: " + state);
             }
         });
-        //Send action off to be executed
+        
+        // Send action off to be executed
         $A.enqueueAction(action);
     },
     
-    clickCreateItem : function(component, event, helper) {
-        var validItem = component.find('campingForm').reduce(function (validSoFar, inputCmp) {
-            //displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        if(validItem){
-            //create the camping item
-            var newItem = component.get("v.newItem");
-            console.log("Create Camping Item: " + JSON.stringify(newItem));  //todo remove after testing
-            helper.createItem(component, newItem);
-        }
-    },
-    handleAddItem : function(component, event, helper) {
-       var newItem = event.getParam("item");
-       helper.addItem(component, newItem);
+    handleAddItem: function(component, event, helper) {
+        var newItem = event.getParam("item");
+        var action = component.get("c.saveItem");
+        action.setParams({"item": newItem});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(item);
+                component.set("v.items",items);
+            }
+        });
+        $A.enqueueAction(action);
+        
     } 
 })
--- force-app/main/default/aura/campingListForm/campingListFormHelper.js
+++ force-app/main/default/aura/campingListForm/campingListFormHelper.js
@@ -1,12 +1,12 @@
 ({
-    createItem: function(component, newItem) {
+    createItem : function(component, campaign) {
         var createEvent = component.getEvent("addItem");
-        createEvent.setParams({ "item": newItem});
+        createEvent.setParams({ "item": campaign });
         createEvent.fire();
         component.set("v.newItem",{ 'sobjectType': 'Camping_Item__c',
-                    'Name': '',
-                    'Quantity__c': 0,
-                    'Price__c': 0,
-                    'Packed__c': false });
+                                   'Name': '',
+                                   'Quantity__c': 0,
+                                   'Price__c': 0,
+                                   'Packed__c': false });
     }
 })
